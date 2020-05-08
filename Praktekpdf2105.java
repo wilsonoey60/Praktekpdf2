22e6ae375fe6faa5783d7ee32e3723e7dd15e068
wilsonoey60 <61810752+wilsonoey60@users.noreply.github.com>
2020-05-08 10:51:18 +0700

    Update Praktekpdf2105.java

--- Praktekpdf2105.java
+++ Praktekpdf2105.java
@@ -7,19 +7,19 @@ class Praktekpdf2105 {
   	String nama, alamat, tipepesawat;
 
   	int bayar, harga;
-    System.out.print("Nama : ");
-    nama = Input.nextLine();
-    System.out.print("Alamat : ");
-    alamat = Input.nextLine();
-    System.out.print("Tipe Pesawat : ");
-    tipepesawat = Input.nextLine();
-    System.out.print("Bayar : ");
-    bayar = Input.nextInt();
-    String karakter1 = "A";
-    String karakter2 = "B";
-    String karakter3 = "C";
+    	System.out.print("Nama : ");
+    	nama = Input.nextLine();
+    	System.out.print("Alamat : ");
+    	alamat = Input.nextLine();
+    	System.out.print("Tipe Pesawat : ");
+    	tipepesawat = Input.nextLine();
+    	System.out.print("Bayar : ");
+    	bayar = Input.nextInt();
+    	String karakter1 = "A";
+    	String karakter2 = "B";
+    	String karakter3 = "C";
     
-    if(tipepesawat == karakter1) {
+    	if(tipepesawat == karakter1) {
    		harga = 5000000;
 	} 
 	else if(tipepesawat == karakter2) {
@@ -29,8 +29,8 @@ class Praktekpdf2105 {
    		harga = 1000000;
 	}
 	harga = Input.nextInt();
-    System.out.println("Nama : " +nama);
-    System.out.println("Alamat : " +alamat);
-    System.out.println("Kembalian : " +(bayar - harga));
+    	System.out.println("Nama : " +nama);
+    	System.out.println("Alamat : " +alamat);
+    	System.out.println("Kembalian : " +(bayar - harga));
   }
 }
